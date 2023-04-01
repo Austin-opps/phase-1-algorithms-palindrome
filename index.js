81079143a87139c142572ef429c61f9b6c99b990
Austin <akomen04@gmail.com>
2023-04-02 01:16:57 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+   function reverseString(word) {
+    return word.split("").reverse().join("");
+   }
+   const reversedString = (reverseString(word));
+   return word === reversedString;
 }
 
 /* 
   Add your pseudocode here
+  reverse the input string
+
+  if the reversed input string is the same as the nput string
+     return true else 
+     return false
 */
 
 /*
   Add written explanation of your solution here
+   the function isPalindrome returns true if when the 
+   reversed string is the same as when its not reversed
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +32,15 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
-}
+
+  console.log("Expecting: True");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("Expecting: True");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("Expecting: False");
+  console.log("=>", isPalindrome("ab"));
+  }
 
 module.exports = isPalindrome;
